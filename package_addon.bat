25a837d46a72e75c48be8304941410a5b05443ed
slippmann <steven.lippmann1@gmail.com>
2020-09-29 16:52:15 -0700

    Fix package script
    Only include behavior and resource folders.

--- package_addon.bat
+++ package_addon.bat
@@ -9,7 +9,7 @@ echo [ OK ]
 
 rem Zip the folders
 <nul set /p ="Zipping... "
-"C:\Program Files\WinRAR\WinRAR.exe" a -r -x*.bat -x.vs* %addon_name%.zip > nul 2>&1 || goto fail
+"C:\Program Files\WinRAR\WinRAR.exe" a -r %addon_name%.zip %addon_name%Res %addon_name%Beh > nul 2>&1 || goto fail
 echo [ OK ]
 
 rem Rename to *.mcaddon
